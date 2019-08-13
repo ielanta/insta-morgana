3a99b37809a387e39a610ad697435e56d3ff14cc
Marina Polyakova <ielanta@yandex.ru>
2019-08-13 13:30:01 +0300

    Refactor schedule

--- core.py
+++ core.py
@@ -1,5 +1,7 @@
 import argparse
 import logging
+import schedule
+import time
 from instagram.parser import InstaAccount
 from mail.mailer import Mailer
 from settings import USER_IDS
@@ -30,24 +32,35 @@ def init_storage(args) -> None:
     storage.set_users_counter(args.usernames)
 
 
-parser = argparse.ArgumentParser(description='Get email notification about instagram new posts and stories')
-subparsers = parser.add_subparsers(help='List of commands')
+def run_schedule(args):
+    schedule.every(8).hours.do(check_media, args)
+    schedule.every().day.at("08:00").do(check_stories, args)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
-media_parser = subparsers.add_parser('media', help='Get email notification about new instagram posts by users')
-media_parser.add_argument('-u', '--usernames', nargs='+', help='instagram usernames')
-media_parser.set_defaults(func=check_media)
 
-stories_parser = subparsers.add_parser('stories', help='Get email notification about new instagram stories by users')
-stories_parser.add_argument('-u', '--usernames', nargs='+', help='instagram usernames')
-stories_parser.set_defaults(func=check_stories)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get email notification about instagram new posts and stories')
+    parser.add_argument('-u', '--usernames', nargs='+', help='instagram usernames', default=USER_IDS)
+    subparsers = parser.add_subparsers(title='actions')
 
-storage_parser = subparsers.add_parser('init', help='Init storage by username: count posts')
-storage_parser.add_argument('-u', '--usernames', nargs='+', help='instagram usernames')
-storage_parser.set_defaults(func=init_storage)
+    media_parser = subparsers.add_parser('media', help='Get email notification about new instagram posts',
+                                         parents=[parser], add_help=False)
+    media_parser.set_defaults(func=check_media)
 
-logging.basicConfig(level=logging.INFO)
-args = parser.parse_args()
-if not args.usernames:
-    args.usernames = USER_IDS
-args.func(args)
+    stories_parser = subparsers.add_parser('stories', help='Get email notification about new instagram stories',
+                                           parents=[parser], add_help=False)
+    stories_parser.set_defaults(func=check_stories)
 
+    storage_parser = subparsers.add_parser('init', help='Init storage by username: count posts',
+                                           parents=[parser], add_help=False)
+    storage_parser.set_defaults(func=init_storage)
+
+    schedule_parser = subparsers.add_parser('schedule', help='Check new posts and stories by schedule',
+                                            parents=[parser], add_help=False)
+    schedule_parser.set_defaults(func=run_schedule)
+
+    logging.basicConfig(level=logging.INFO)
+    params = parser.parse_args()
+    params.func(params)
